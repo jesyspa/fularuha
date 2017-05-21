0563748d56b555345fa50fbb108570489b5138ea
Anton Golov <jesyspa@gmail.com>
2017-05-21 18:59:22 +0200

    Added running from file

--- runtime/src/main.rs
+++ runtime/src/main.rs
@@ -9,4 +9,20 @@ mod eval_context;
 #[cfg(test)]
 mod tests;
 
-fn main() { }
+use std::env;
+use std::fs::File;
+use std::io::Read;
+extern crate serde_json;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Please provide a file to run");
+    } else {
+        let mut file = File::open(&args[1]).expect("invalid file");
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).expect("read failed");
+        let code: Vec<bytecode::Inst> = serde_json::from_str(&contents).expect("decode failed");
+        evaluator::evaluate(&code);
+    }
+}
