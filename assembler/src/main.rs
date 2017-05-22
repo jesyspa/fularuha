f3d3d37d85d4c0b7983f691078bc9e6f4cc9b02b
Anton Golov <jesyspa@gmail.com>
2017-05-22 11:06:23 +0200

    Added output file specification for assembler

--- assembler/src/main.rs
+++ assembler/src/main.rs
@@ -14,27 +14,39 @@ mod assembler;
 use parser::parse;
 use assembler::assemble;
 
-use std::env;
 use std::fs::File;
 use std::io::Read;
 use std::io::Write;
+
 extern crate serde_json;
 
+extern crate clap;
+use clap::{Arg, App};
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        println!("Please provide a file to assemble");
-    } else {
-        let mut infile = File::open(&args[1]).expect("invalid file");
-        let mut contents = String::new();
-        infile.read_to_string(&mut contents).expect("read failed");
-        let parsed_input = parse(&contents);
+    let matches = App::new("FuLaRuHa assembler")
+        .arg(Arg::with_name("INPUT")
+             .help("File to compile")
+             .required(true)
+             .index(1))
+        .arg(Arg::with_name("output")
+             .long("output")
+             .short("o")
+             .value_name("FILE")
+             .takes_value(true))
+        .get_matches();
 
-        let assembled_output = assemble(&parsed_input);
+    let infile_name = matches.value_of("INPUT").expect("no input provided");
+    let mut infile = File::open(infile_name).expect("invalid file");
+    let mut contents = String::new();
+    infile.read_to_string(&mut contents).expect("read failed");
+    let parsed_input = parse(&contents);
 
-        let mut outfile = File::create("output.json").expect("failed to open file");
-        let json = serde_json::to_string(&assembled_output).expect("failed to encode");
-        outfile.write(json.as_bytes()).expect("write failed");
-    }
+    let assembled_output = assemble(&parsed_input);
+
+    let outfile_name = matches.value_of("output").unwrap_or("output.json");
+    let mut outfile = File::create(outfile_name).expect("failed to create file");
+    let json = serde_json::to_string(&assembled_output).expect("failed to encode");
+    outfile.write(json.as_bytes()).expect("write failed");
 }
 
